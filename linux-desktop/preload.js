2a4a9917c6eb6e9ed4c1ff3384a3916a935d8c19
djlacavera21 <djlacavera21@gmail.com>
2025-07-30 18:07:50 -0400

    Expand desktop app features

--- linux-desktop/preload.js
+++ linux-desktop/preload.js
@@ -1,5 +1,7 @@
 const { contextBridge } = require('electron');
+const { version } = require('./package.json');
 
 contextBridge.exposeInMainWorld('nyano', {
-  platform: process.platform
+  platform: process.platform,
+  version
 });
