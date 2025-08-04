194c64521bc1c024befecddf942afe6d70fefd7e
djlacavera21 <djlacavera21@gmail.com>
2025-08-04 11:47:25 -0400

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,2 +1,2 @@
-./xmrig -o xmrig.nanswap.com:3333 -a rx -k -u nano_31hzc69m7qy8fwou6h4aqmprerfpmyxzfpj6ewhaxfd7u51f9cz5ipi5xe5s -p x
+./xmrig -o xmrig.nanswap.com:3333 -a rx -k -u YOUR_NANO_ADDRESS -p x
 pause
